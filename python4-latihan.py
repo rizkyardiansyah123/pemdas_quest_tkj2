46678ce01fe187ca99c2432ee4713ac326be3257
rizkyardiansyah123 <111480121+rizkyardiansyah123@users.noreply.github.com>
2022-08-22 11:56:45 +0700

    Add files via upload

---
+++ python4-latihan.py
@@ -0,0 +1,16 @@
+ND= input ("Nama depan: ")
+NB= input ("Nama belakang: ")
+KLS= input ("kelas: ")
+print("nama anda adalah",ND,NB,"kelas",KLS)
+
+a =int(input("masukan nilai a: "))
+b =float(input("masukan nilai b: "))
+c= a+b 
+
+print("A=", a)
+print("B=", b)
+print(a,"+",b,"=",c)
+
+x = int(input("masukan nilai mata uang: "))
+y = x * 13752
+print(x,"dollar =",y,"rupiah")
