910c7ea45ed59c04f0e88b42efdec778bbe3f70e
rizkyardiansyah123 <111480121+rizkyardiansyah123@users.noreply.github.com>
2022-08-22 13:27:29 +0700

    Add files via upload

---
+++ python4-Rizkyard.py
@@ -0,0 +1,27 @@
+print("=====================")
+print("Rumus Bangun Datar")
+print("====================\n")
+
+print("1. persegi\n")
+
+sisi = int(input("Masukan Nilai Sisi: "))
+luas_p = sisi * sisi 
+print("luas persegi adalah",luas_p)
+
+print("2.persegi panjang")
+
+panjang = int(input("masukan nilai panjang: "))
+lebar = int(input("masukan nilai lebar: "))
+luas_pp = panjang * lebar
+print("luas persegi panjang",luas_pp)
+
+print("3.segitiga")
+
+alas = int(input("masukan nilai alas: "))
+tinggi = int(input("masukan nilai tinggi: "))
+luas_s = alas * tinggi / 2
+
+print("luas segitiga adalah: ",luas_s)
+
+
+
