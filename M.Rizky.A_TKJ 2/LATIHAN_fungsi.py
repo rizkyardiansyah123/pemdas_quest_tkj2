c304aee6bfdd1671abf1325b9d24e4b9ecdc08b5
rizkyardiansyah123 <111480121+rizkyardiansyah123@users.noreply.github.com>
2023-02-13 09:40:34 +0700

    Add files via upload

---
+++ M.Rizky.A_TKJ 2/LATIHAN_fungsi.py
@@ -0,0 +1,38 @@
+'''Fuungsi pada python didefinisikan dengan def'''
+
+'''def nama_fungsi():'''
+
+def salam():
+    print("assalamu'alaikum !! akhi ukhty")
+
+salam()
+
+'''Buatlah sebuah fungsi versi kalian'''
+
+def punten():
+    print("samlekom mamank")
+
+punten()
+
+'''Fungsi dengan parameter'''
+'''def nama_fungsi(p1,p2)'''
+
+def say_hi(nama):
+    print("Halo perkenalkan nama saya", nama)
+
+say_hi("jawir")
+say_hi("Masbruh")
+
+'''Buatlah fungsi dengan 3 parameter'''
+
+def say_hi(nama,umur,kelas):
+    pass
+    print("Halo nama saya:",nama)
+    print("Umur saya:",umur)
+    print("kelas saya:",kelas)
+
+say_hi("Masbruh","200","30 TKJ 5")
+
+print("\n\n======== BATAS ========\n\n")
+
+say_hi("Jawir","100"," 50 TKJ 6")
